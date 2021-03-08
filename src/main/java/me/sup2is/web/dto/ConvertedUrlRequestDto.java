7c034cd05893461c26180f144cde75d695addd9c
sup2is <dev.sup2is@gmail.com>
2021-03-08 23:54:59 +0900

    orgUrl @NotBlank로 변경

--- src/main/java/me/sup2is/web/dto/ConvertedUrlRequestDto.java
+++ src/main/java/me/sup2is/web/dto/ConvertedUrlRequestDto.java
@@ -6,14 +6,15 @@ import lombok.NoArgsConstructor;
 import me.sup2is.domain.ConvertedUrl;
 import org.hibernate.validator.constraints.URL;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ConvertedUrlRequestDto {
 
-    @NotNull @URL(message = "Unable to shorten that link. It is not a valid url.")
+    @NotBlank
+    @URL(message = "Unable to shorten that link. It is not a valid url.")
     private String orgUrl;
 
     public ConvertedUrl toConvertedUrl() {
